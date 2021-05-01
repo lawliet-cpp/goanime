8c0452d6e7aef6b39d7cfe8a219a36180259a721
Aymen <60004748+onizuka341@users.noreply.github.com>
2021-05-01 17:50:48 +0100

    Update api.go

--- api.go
+++ api.go
@@ -4,36 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func Download(c *fiber.Ctx) error {
 
-	link, err := DownloadLinkAr(c.Query("anime"), c.Query("episode"))
-
-	if err != nil {
-		return c.JSON(&fiber.Map{
-			"error": "cannot get the episode",
-		})
-	}
-
-	image, name := AnimeAvarar(c.Query("anime"))
-	return c.JSON(&fiber.Map{
-		"link":  link,
-		"image": image,
-		"name":  name,
-	})
-}
-
-func Watch(c *fiber.Ctx) error {
-
-	link := EpisodeLinkAr(c.Query("anime"), c.Query("episode"))
-	image, name := AnimeAvarar(c.Query("anime"))
-
-	return c.JSON(&fiber.Map{
-		"link":  link,
-		"image": image,
-		"name":  name,
-	})
-
-}
 
 func Search(c *fiber.Ctx) error {
 	name := c.Params("anime")
@@ -46,14 +17,13 @@ func AnimeByName(c *fiber.Ctx) error {
 	return c.JSON(anime)
 }
 func Handler(app *fiber.App) {
-	app.Get("/anime/download", Download)
-	app.Get("/anime/watch", Watch)
+	
 	app.Get("/anime/:anime", AnimeByName)
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(&fiber.Map{
-			"Watch":        "/anime/watch",
-			"Download":     "/anime/downloads",
-			"Query params": "anime:String , Episode:Integer",
+			"List":        "/anime/search/:query",
+			"Single Result":     "/anime/:query",
+			
 		})
 	})
 	app.Get("/anime/search/:anime", Search)
